87a8b3c224298616b64b1bf03d2b7cff42d69dff
vxzyfx's github <vxzyfx@gmail.com>
2024-06-22 23:12:09 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,28 +4,33 @@
 if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
 
   # 设置 Github CDN 及若干变量，如是 IPv6 only 或者大陆机器，需要 Github 加速网，可自行查找放在 GH_PROXY 处 ，如 https://mirror.ghproxy.com/ ，能不用就不用，减少因加速网导致的故障。
-  GH_PROXY='https://ghproxy.lvedong.eu.org/'
+  #GH_PROXY='https://ghproxy.lvedong.eu.org/'
   GRPC_PROXY_PORT=443
   GRPC_PORT=5555
   WEB_PORT=80
   CADDY_HTTP_PORT=2052
   WORK_DIR=/dashboard
 
+  # 默认agent的证书和dashboard的一样
+  NEZHA_AG_CERT_PEM=${NEZHA_AG_CERT_PEM:-$NEZHA_CERT_PEM}
+  NEZHA_AG_KEY_PEM=${NEZHA_AG_KEY_PEM:-$NEZHA_KEY_PEM}
+
   # 如不分离备份的 github 账户，默认与哪吒登陆的 github 账户一致
   GH_BACKUP_USER=${GH_BACKUP_USER:-$GH_USER}
+  REVERSE_PROXY_MODE=${REVERSE_PROXY_MODE:-nginx}
 
   error() { echo -e "\033[31m\033[01m$*\033[0m" && exit 1; } # 红色
   info() { echo -e "\033[32m\033[01m$*\033[0m"; }   # 绿色
   hint() { echo -e "\033[33m\033[01m$*\033[0m"; }   # 黄色
 
   # 如参数不齐全，容器退出，另外处理某些环境变量填错后的处理
-  [[ -z "$GH_USER" || -z "$GH_CLIENTID" || -z "$GH_CLIENTSECRET" || -z "$ARGO_AUTH" || -z "$ARGO_DOMAIN" ]] && error " There are variables that are not set. "
+  [[ -z "$GH_USER" || -z "$GH_CLIENTID" || -z "$GH_CLIENTSECRET" || -z "$ARGO_AUTH" || -z "$AG_DOMAIN" ]] && error " There are variables that are not set. "
   [[ "$ARGO_AUTH" =~ TunnelSecret ]] && grep -qv '"' <<< "$ARGO_AUTH" && ARGO_AUTH=$(sed 's@{@{"@g;s@[,:]@"\0"@g;s@}@"}@g' <<< "$ARGO_AUTH")  # Json 时，没有了"的处理
   [[ "$ARGO_AUTH" =~ ey[A-Z0-9a-z=]{120,250}$ ]] && ARGO_AUTH=$(awk '{print $NF}' <<< "$ARGO_AUTH") # Token 复制全部，只取最后的 ey 开始的
   [ -n "$GH_REPO" ] && grep -q '/' <<< "$GH_REPO" && GH_REPO=$(awk -F '/' '{print $NF}' <<< "$GH_REPO")  # 填了项目全路径的处理
 
   # 检测是否需要启用 Github CDN，如能直接连通，则不使用
-  [ -n "$GH_PROXY" ] && wget --server-response --quiet --output-document=/dev/null --no-check-certificate --tries=2 --timeout=3 https://raw.githubusercontent.com/fscarmen2/Argo-Nezha-Service-Container/main/README.md >/dev/null 2>&1 && unset GH_PROXY
+  [ -n "$GH_PROXY" ] && wget --server-response --quiet --output-document=/dev/null --no-check-certificate --tries=2 --timeout=3 https://raw.githubusercontent.com/vxzyfx/nezha-service/main/README.md >/dev/null 2>&1 && unset GH_PROXY
 
   # 设置 DNS
   echo -e "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\nnameserver 2001:4860:4860::8844\nnameserver 2400:3200::1\n" > /etc/resolv.conf
@@ -50,7 +55,7 @@ if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
 
   # 用户选择使用 gRPC 反代方式: Nginx / Caddy / grpcwebproxy，默认为 Caddy；如需使用 grpcwebproxy，把 REVERSE_PROXY_MODE 的值设为 nginx 或 grpcwebproxy
   if [ "$REVERSE_PROXY_MODE" = 'grpcwebproxy' ]; then
-    wget -c ${GH_PROXY}https://github.com/fscarmen2/Argo-Nezha-Service-Container/releases/download/grpcwebproxy/grpcwebproxy-linux-$ARCH.tar.gz -qO- | tar xz -C $WORK_DIR
+    wget -c ${GH_PROXY}https://github.com/vxzyfx/nezha-service/releases/download/grpcwebproxy/grpcwebproxy-linux-$ARCH.tar.gz -qO- | tar xz -C $WORK_DIR
     chmod +x $WORK_DIR/grpcwebproxy
     GRPC_PROXY_RUN="$WORK_DIR/grpcwebproxy --server_tls_cert_file=$WORK_DIR/nezha.pem --server_tls_key_file=$WORK_DIR/nezha.key --server_http_tls_port=$GRPC_PROXY_PORT --backend_addr=localhost:$GRPC_PORT --backend_tls_noverify --server_http_max_read_timeout=300s --server_http_max_write_timeout=300s"
   elif [ "$REVERSE_PROXY_MODE" = 'nginx' ]; then
@@ -65,16 +70,40 @@ events {
         # multi_accept on;
 }
 http {
+
+server {
+    listen $GRPC_PROXY_PORT ssl http2;
+    listen [::]:$GRPC_PROXY_PORT ssl http2;
+    ssl_certificate          $WORK_DIR/nezha-dashboard.pem;
+    ssl_certificate_key      $WORK_DIR/nezha-dashboard.key;
+    server_name $DASHBOARD_DOMAIN;
+    location / {
+        proxy_pass localhost:$WEB_PORT;
+        proxy_set_header Host $http_host;
+        proxy_set_header      Upgrade $http_upgrade;
+    }
+    location ~ ^/(ws|terminal/.+)$  {
+        proxy_pass localhost:$WEB_PORT;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $http_host;
+    }
+}
   upstream grpcservers {
     server localhost:$GRPC_PORT;
     keepalive 1024;
   }
   server {
-    listen 127.0.0.1:$GRPC_PROXY_PORT ssl http2;
-    server_name $ARGO_DOMAIN;
-    ssl_certificate          $WORK_DIR/nezha.pem;
-    ssl_certificate_key      $WORK_DIR/nezha.key;
+    listen $GRPC_PROXY_PORT ssl http2;
+    listen [::]:$GRPC_PROXY_PORT ssl http2;
+    server_name $AG_DOMAIN;
+    ssl_certificate          $WORK_DIR/nezha-ag.pem;
+    ssl_certificate_key      $WORK_DIR/nezha-ag.key;
     underscores_in_headers on;
+    keepalive_time 24h;
+    keepalive_requests 100000;
+    keepalive_timeout 120s;
     location / {
       grpc_read_timeout 300s;
       grpc_send_timeout 300s;
@@ -124,7 +153,7 @@ Debug: false
 HTTPPort: $WEB_PORT
 Language: zh-CN
 GRPCPort: $GRPC_PORT
-GRPCHost: $ARGO_DOMAIN
+GRPCHost: $AG_DOMAIN
 ProxyGRPCPort: $GRPC_PROXY_PORT
 TLS: true
 Oauth2:
@@ -140,7 +169,7 @@ site:
 EOF
 
   # 下载包含本地数据的 sqlite.db 文件，生成18位随机字符串用于本地 Token
-  wget -P ${WORK_DIR}/data/ ${GH_PROXY}https://github.com/fscarmen2/Argo-Nezha-Service-Container/raw/main/sqlite.db
+  wget -P ${WORK_DIR}/data/ ${GH_PROXY}https://github.com/vxzyfx/nezha-service/raw/main/sqlite.db
   LOCAL_TOKEN=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 18)
   sqlite3 ${WORK_DIR}/data/sqlite.db "update servers set secret='${LOCAL_TOKEN}' where created_at='2023-04-23 13:02:00.770756566+08:00'"
 
@@ -162,16 +191,16 @@ credentials-file: $WORK_DIR/argo.json
 protocol: http2
 
 ingress:
-  - hostname: $ARGO_DOMAIN
+  - hostname: $AG_DOMAIN
     service: https://localhost:$GRPC_PROXY_PORT
     path: /proto.NezhaService/*
     originRequest:
       http2Origin: true
       noTLSVerify: true
-  - hostname: $ARGO_DOMAIN
+  - hostname: $AG_DOMAIN
     service: ssh://localhost:22
     path: /$GH_CLIENTID/*
-  - hostname: $ARGO_DOMAIN
+  - hostname: $AG_DOMAIN
     service: http://localhost:$WEB_PORT
   - service: http_status:404
 EOF
@@ -181,10 +210,11 @@ EOF
     ARGO_RUN="cloudflared tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH}"
   fi
 
-  # 生成自签署SSL证书
-  openssl genrsa -out $WORK_DIR/nezha.key 2048
-  openssl req -new -subj "/CN=$ARGO_DOMAIN" -key $WORK_DIR/nezha.key -out $WORK_DIR/nezha.csr
-  openssl x509 -req -days 36500 -in $WORK_DIR/nezha.csr -signkey $WORK_DIR/nezha.key -out $WORK_DIR/nezha.pem
+  # 生成SSL证书
+  echo $NEZHA_CERT_PEM > $WORK_DIR/nezha-dashboard.key
+  echo $NEZHA_KEY_PEM > $WORK_DIR/nezha-dashboard.pem
+  echo $NEZHA_AG_CERT_PEM > $WORK_DIR/nezha-ag.key
+  echo $NEZHA_AG_KEY_PEM > $WORK_DIR/nezha-ag.pem
 
   # 生成 backup.sh 文件的步骤1 - 设置环境变量
   cat > $WORK_DIR/backup.sh << EOF
@@ -206,7 +236,7 @@ IS_DOCKER=1
 EOF
 
   # 生成 backup.sh 文件的步骤2 - 在线获取 template/bakcup.sh 模板生成完整 backup.sh 文件
-  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/fscarmen2/Argo-Nezha-Service-Container/main/template/backup.sh | sed '1,/^########/d' >> $WORK_DIR/backup.sh
+  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/vxzyfx/nezha-service/main/template/backup.sh | sed '1,/^########/d' >> $WORK_DIR/backup.sh
 
   if [[ -n "$GH_BACKUP_USER" && -n "$GH_EMAIL" && -n "$GH_REPO" && -n "$GH_PAT" ]]; then
     # 生成 restore.sh 文件的步骤1 - 设置环境变量
@@ -228,7 +258,7 @@ IS_DOCKER=1
 EOF
 
     # 生成 restore.sh 文件的步骤2 - 在线获取 template/restore.sh 模板生成完整 restore.sh 文件
-    wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/fscarmen2/Argo-Nezha-Service-Container/main/template/restore.sh | sed '1,/^########/d' >> $WORK_DIR/restore.sh
+    wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/vxzyfx/nezha-service/main/template/restore.sh | sed '1,/^########/d' >> $WORK_DIR/restore.sh
   fi
 
   # 生成 renew.sh 文件的步骤1 - 设置环境变量
@@ -243,7 +273,7 @@ TEMP_DIR=/tmp/renew
 EOF
 
   # 生成 renew.sh 文件的步骤2 - 在线获取 template/renew.sh 模板生成完整 renew.sh 文件
-  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/fscarmen2/Argo-Nezha-Service-Container/main/template/renew.sh | sed '1,/^########/d' >> $WORK_DIR/renew.sh
+  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/vxzyfx/nezha-service/main/template/renew.sh | sed '1,/^########/d' >> $WORK_DIR/renew.sh
 
   # 生成定时任务: 1.每天北京时间 3:30:00 更新备份和还原文件，2.每天北京时间 4:00:00 备份一次，并重启 cron 服务； 3.每分钟自动检测在线备份文件里的内容
   [ -z "$NO_AUTO_RENEW" ] && [ -s $WORK_DIR/renew.sh ] && ! grep -q "$WORK_DIR/renew.sh" /etc/crontab && echo "30 3 * * * root bash $WORK_DIR/renew.sh" >> /etc/crontab
